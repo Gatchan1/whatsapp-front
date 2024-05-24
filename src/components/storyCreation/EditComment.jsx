d300cffd8187ddf0377ef3a636c61f12598687df
Gatchan1 <r.barrio.f@gmail.com>
2024-05-24 14:27:54 +0200

    update comment works

--- src/components/storyCreation/EditComment.jsx
+++ src/components/storyCreation/EditComment.jsx
@@ -1,13 +1,71 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { storyCreationContext } from "../../contexts/storyCreation.context";
 
-export default function EditComment({comment}) {
-    const[value, setValue] = useState(comment);
+export default function EditComment({ comment, index }) {
+  const { story, setStory, storyCopy, retrieveUniqueAuthors } = useContext(storyCreationContext);
+  const [value, setValue] = useState(comment);
+  const [showOptions, setShowOptions] = useState(false);
+  const [bgColor, setBgColor] = useState("set");
+
+  const updateMessage = () => {
+    const newStory = storyCopy();
+    newStory[index][3] = value;
+    setStory(newStory);
+    setBgColor("set");
+  };
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      updateMessage();
+    }
+  };
+
+  function Options() {
+    return (
+      <div className="inline">
+        <button type="button" className={"enter option " + (bgColor === "unset" ? "active" : "disabled")} disabled={bgColor === "set" ? "true" : ""} onMouseDown={updateMessage}>
+          ✔️
+        </button>
+        <button
+          type="button"
+          className={"undo option " + (bgColor === "unset" ? "active" : "disabled")}
+          disabled={bgColor === "set" ? "true" : ""}
+          //(writing "false" returns true for being a non-empty string)
+          onMouseDown={() => {
+            //used onMouseDown instead of onClick because it triggers before onBlur.
+            setBgColor("set");
+            setValue(comment);
+            setShowOptions(false);
+          }}
+        >
+          ↶
+        </button>
+      </div>
+    );
+  }
 
   return (
-    <form>
-    <input size={value.length} value={value} onChange={(e) => setValue(e.target.value)}/>
+    <form onBlur={() => setShowOptions(false)}>
+      <input
+        size={value.length}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+          if (e.target.value != comment) {
+            setBgColor("unset");
+          } else {
+            setBgColor("set");
+          }
+        }}
+        onFocus={() => setShowOptions(true)}
+        onKeyDown={handleEnter}
+        className={bgColor}
+      />
+      {showOptions && <Options />}
     </form>
-  )
+  );
 }
 
 //TODO: lots!! have to end up updating story!!
+//add onKeyDown!!!
--- src/components/storyCreation/Message.jsx
+++ src/components/storyCreation/Message.jsx
@@ -8,7 +8,7 @@ export default function Message({ message, index }) {
       <EditTime checkbox={message[0]} time={message[1]} index={index} />
       <div className="row left-margin">
         <EditAuthorMessage author={message[2]} index={index}/>
-        <EditComment comment={message[3]} />
+        <EditComment comment={message[3]} index={index}/>
       </div>
     </div>
   );
