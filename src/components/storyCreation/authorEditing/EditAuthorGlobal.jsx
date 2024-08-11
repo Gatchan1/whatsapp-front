5c3b8438a725b2fa638489d2464516a4937b8b92
Gatchan1 <r.barrio.f@gmail.com>
2024-08-11 15:19:07 +0200

    moved to folder

---
+++ src/components/storyCreation/authorEditing/EditAuthorGlobal.jsx
@@ -0,0 +1,34 @@
+import { useContext, useEffect, useState } from "react";
+import { storyCreationContext } from "../../../contexts/storyCreation.context";
+
+export default function EditAuthorGlobal({author}) {
+    const { story, setStory, storyCopy, retrieveUniqueAuthors } = useContext(storyCreationContext);
+    const [value, setValue] = useState(author);
+    
+    useEffect(()=>{
+        setValue(author);
+    },[story, author])
+    
+    const updateAll = () => {
+        const newStory = storyCopy();
+        for (let i = 0; i < story.length; i++) {
+            if (newStory[i][2] == author) {
+                newStory[i][2] = value;
+            }
+        }
+        setStory(newStory);
+        retrieveUniqueAuthors(newStory);
+    }
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') {
+            updateAll();
+        }
+    }
+
+  return (
+    <form className="author" onSubmit={(e) => e.preventDefault()}>
+    <input onChange={(e) => setValue(e.target.value)} onKeyDown={handleEnter} size={value.length} value={value}/>
+    </form>
+  )
+}
