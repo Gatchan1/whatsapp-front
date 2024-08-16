83874f5a830abbae29dd46b428599aeeb13cbce3
Gatchan1 <r.barrio.f@gmail.com>
2024-08-16 19:08:33 +0200

    refactoring

--- src/components/storyCreation/StoryCopyPaste.jsx
+++ src/components/storyCreation/StoryCopyPaste.jsx
@@ -1,38 +1,35 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { storyCreationContext } from "../../contexts/storyCreation.context";
 
 export default function StoryCopyPaste({ setCopyPasting }) {
   const { setStory } = useContext(storyCreationContext);
   const [storyText, setStoryText] = useState("");
-  const [messages, setMessages] = useState([]);
-  
-  useEffect(() => {
-    if (messages[0]) {
-      buildStory();
-      setCopyPasting(false); // this component will cease to be shown (see CreatePage.jsx)
-    }
-  }, [messages]);
 
-  const dateTime = /\[\d{1,2}:\d{2}, \d{1,2}\/\d{1,2}\/\d{4}\]/;
-  // [0:01, 28/3/2024]
+  const dateTime = /\[\d{1,2}:\d{2}, \d{1,2}\/\d{1,2}\/\d{4}\]/; // [0:01, 28/3/2024]
   const dateTimeGlobal = /\[\d{1,2}:\d{2}, \d{1,2}\/\d{1,2}\/\d{4}\]/g;
 
-  //TODO: proteger la entrada de datos que no encajen!!!
-  const buildStory = () => {
-    const newStory = [];
-    const times = storyText.match(dateTimeGlobal);
+  const handleBuildStory = (e) => {
+    e.preventDefault();
+    if (!storyText) {
+      setCopyPasting(false); // this component will cease to be shown (see CreatePage.jsx)
+      return;
+    }
+    const splitStoryText = storyText.split(dateTime);
+    splitStoryText.shift();
+    //first index of the array would otherwise always be empty due to split.
+    const messages = splitStoryText.map((string) => string.trim());
+    //each string started with a single space and had a newline character at the end '\n'.
 
+    const times = storyText.match(dateTimeGlobal);
     const dates = times.map((time) => {
       const noBrackets = time.slice(1, time.length - 1);
       const [timeString, dateStringWithoutSpace] = noBrackets.split(", ");
       let [hours, minutes] = timeString.split(":");
       const [day, month, year] = dateStringWithoutSpace.split("/");
-      // return new Date(year, month - 1, day, hours, minutes);
       if (minutes.length != 2) minutes = "0" + minutes;
       return new Date(`${year}-${month}-${day} ${hours}:${minutes}`);
-      // return {year, month, day, hours, minutes};
     });
-    // console.log("dateees:", dates);
+
     const authors = messages.map((message) => {
       const index = message.search(/:\s/);
       return message.slice(0, index);
@@ -42,6 +39,8 @@ export default function StoryCopyPaste({ setCopyPasting }) {
       const index = message.search(/:\s/);
       return message.slice(index + 2);
     });
+
+    const newStory = [];
     for (let i = 0; i < messages.length; i++) {
       newStory[i] = [];
       newStory[i][0] = false; //checkbox for date modification
@@ -50,19 +49,8 @@ export default function StoryCopyPaste({ setCopyPasting }) {
       newStory[i][3] = authors[i];
       newStory[i][4] = comments[i];
     }
-    // console.log("storyyy", newStory);
     setStory(newStory);
-  };
-
-  const handleRestructuring = (e) => {
-    e.preventDefault();
-    if (!storyText) return;
-
-    const splitStory = storyText.split(dateTime);
-    splitStory.shift();
-    //first index of the array would otherwise always be empty due to split.
-    setMessages(splitStory.map((string) => string.trim()));
-    //each string started with a single space and had a newline character at the end '\n'.
+    setCopyPasting(false); // this component will cease to be shown (see CreatePage.jsx)
   };
 
   return (
@@ -71,7 +59,7 @@ export default function StoryCopyPaste({ setCopyPasting }) {
         <h3>StoryCopyPaste</h3>
         <form>
           <textarea className="copy-paste" rows="20" cols="50" placeholder="*Paste your desired whatsapp conversation fragment here*" onChange={(e) => setStoryText(e.target.value)} />
-          <button onClick={handleRestructuring}>Start</button>
+          <button onClick={handleBuildStory}>Start</button>
         </form>
       </div>
     </div>
